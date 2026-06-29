07d258c0734973469d17cb5652c21c8c90389a7c
Nemanja Jevtić <njevtic22@gmail.com>
2026-06-29 11:25:34 +0200

    Implemented filtering users

--- book-api/book-rails/app/controllers/api/users_controller.rb
+++ book-api/book-rails/app/controllers/api/users_controller.rb
@@ -1,9 +1,12 @@
 class Api::UsersController < ApplicationController
   def index
     page, size = get_pagination_params
-    pagination_metadata = get_pagination_metadata(records: User.all, size: size)
 
-    result = UserSerializer.as_json(User.offset(page * size).limit(size))
+    users = UsersQuery.build(User.all, params)
+
+    pagination_metadata = get_pagination_metadata(records: users, size: size)
+
+    result = UserSerializer.as_json(users.offset(page * size).limit(size))
     render json: pagination_metadata.merge(data: result)    # creates new hash
     # render json: pagination_metadata.merge!(data: result) # mutates the original
   end
---
+++ book-api/book-rails/app/queries/users_query.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class UsersQuery
+  FILTERABLE_FIELDS = %w[name surname email username role].freeze
+
+  def self.build(fetch_relation, params)
+    fetch_relation = filter(fetch_relation, params)
+    fetch_relation
+  end
+
+  private
+
+  def self.filter(relation, params)
+    FILTERABLE_FIELDS.each do |field|
+      next unless params[field].present?
+
+      relation = relation.where("#{field} ILIKE ?", "%#{params[field]}%")
+    end
+
+    relation
+  end
+
+  def sort(fetch_relation, params)
+    # TODO
+  end
+end
