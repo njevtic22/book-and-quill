41b459a55a169d298501aeddff8b9bd7f547099e
Nemanja Jevtić <njevtic22@gmail.com>
2026-07-01 15:34:11 +0200

    Implemented sorting users by single field

--- book-api/book-rails/app/controllers/api/users_controller.rb
+++ book-api/book-rails/app/controllers/api/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::UsersController < ApplicationController
   def index
+    # TODO: add sorting
     page, size = get_pagination_params
+    # TODO: don't pass whole params
     users = UserSearch.build(User.all, params)
     result = Paginator.paginate(page, size, users)
     result.data = UserSerializer.as_json(result.data)
--- book-api/book-rails/app/queries/user_search.rb
+++ book-api/book-rails/app/queries/user_search.rb
@@ -2,25 +2,39 @@
 
 class UserSearch
   FILTERABLE_FIELDS = %w[name surname email username role].freeze
+  SORTABLE_FIELDS = %w[id name surname email username role].freeze
+  SORT_DIRECTIONS = %w[asc desc].freeze
 
-  def self.build(fetch_relation, params)
-    fetch_relation = filter(fetch_relation, params)
-    fetch_relation
+  def self.build(records, params)
+    records = filter(records, params)
+    sort(records, params[:sort])
   end
 
   private
 
-  def self.filter(relation, params)
+  def self.filter(records, params)
     FILTERABLE_FIELDS.each do |field|
       next unless params[field].present?
 
-      relation = relation.where("#{field} ILIKE ?", "%#{params[field]}%")
+      records = records.where("#{field} ILIKE ?", "%#{params[field]}%")
     end
-
-    relation
+    records
   end
 
-  def sort(fetch_relation, params)
-    # TODO
+  def self.sort(records, sort_by)
+    return records.order(id: :asc) if sort_by.blank?
+
+    field, direction, extra = sort_by.to_s.split(",")
+    field = field&.downcase
+    direction = direction&.downcase
+
+    # TODO: move validation to ApplicationController
+    raise InvalidSortError, "Sort must be in format `<field>,<direction>" if field.blank? || direction.blank? || extra
+    raise InvalidSortError, "invalid sort field '#{field}'" unless SORTABLE_FIELDS.include?(field)
+    raise InvalidSortError, "Invalid sort direction '#{direction}'" unless SORT_DIRECTIONS.include?(direction)
+
+    records.order(field => direction)
   end
+
+  class InvalidSortError < StandardError; end
 end
